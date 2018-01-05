2a29c59d3cd72a10f4c5ce157e91ef1d2ef91022
Nelson Izquierdo <nelson@mitocondria.cl>
2018-01-05 14:33:11 -0300

    first commit

---
+++ lugares.js
@@ -0,0 +1,64 @@
+eqfeed_callback (
+{
+
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.63041145331226,
+          -33.455755068172564
+        ]
+      },
+      "properties": {
+          "name":"Mariale",
+          "Direccion":"Manuel Antonio Matta 52"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.6009150436148,
+          -33.44712680642698
+        ]
+      },
+      "properties": {
+          "name":"Nelson",
+          "Direccion":"Suecia 2955"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.57766276411712,
+          -33.45396592973067
+        ]
+      },
+      "properties": {
+          "name":"Pedro y Mafe",
+          "Direccion":"Irarrazaval 5000"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.59437592281029,
+          -33.45637381727903
+        ]
+      },
+      "properties": {
+          "name":"Seguridad y Simón",
+          "Direccion":"Plaza Ñuñoa"
+      }
+    }
+  ]
+}
+)
